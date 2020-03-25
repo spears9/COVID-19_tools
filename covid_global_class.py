412c46e014fce76206b6f14e4534f0e41a867650
Brian Spears <spears9@llnl.gov>
2020-03-25 16:54:07 -0700

    introduce covid_global_class.py for general analysis of JHU data; deprecate covid_class.py (JHU changed format,etc.)

---
+++ covid_global_class.py
@@ -0,0 +1,82 @@
+# read COVID-19 data from:
+# https://github.com/CSSEGISandData/COVID-19.git
+# load data, find histories by region
+# find logarithmic derivatives
+
+# my local repo: /Users/repos/COVID-19
+
+from pylab import *
+import csv
+ion()
+
+filePath      = '/Users/spears9/repos/COVID-19/csse_covid_19_data/csse_covid_19_time_series/'
+dammit = 1
+def getData(filePath):
+    #confirmedFile = 'time_series_19-covid-Confirmed.csv'
+    #deathsFile    = 'time_series_19-covid-Deaths.csv'
+    #recoveredFile = 'time_series_19-covid-Recovered.csv'
+    confirmedFile = 'time_series_covid19_confirmed_global.csv'
+    deathsFile = 'time_series_covid19_deaths_global.csv'
+    state     = []
+    country   = []
+    data      = []
+    with open(filePath+confirmedFile,'r') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        heads = spamreader.next()
+        for row in spamreader:
+            data.append(row)
+            state.append(row[0])
+            country.append(row[1])
+    ncol  = len(heads)-2
+    #    confirmed = loadtxt(filePath+confirmedFile,delimiter=',',skiprows=1,usecols= tuple(arange(4,ncol+2,1)))
+    #    recovered = loadtxt(filePath+recoveredFile,delimiter=',',skiprows=1,usecols= tuple(arange(4,ncol+2,1)))
+    #    deaths    = loadtxt(filePath+deathsFile,delimiter=',',skiprows=1,usecols= tuple(arange(4,ncol+2,1)))
+    # hack to fix broken CSSE data
+    confirmed = loadtxt(filePath+confirmedFile,delimiter=',',skiprows=1,usecols= tuple(arange(4,ncol+2,1)))
+    #recovered = loadtxt(filePath+recoveredFile,delimiter=',',skiprows=1,usecols= tuple(arange(4,ncol+2-1,1)))
+    deaths    = loadtxt(filePath+deathsFile,delimiter=',',skiprows=1,usecols= tuple(arange(4,ncol+2,1)))
+    current   = confirmed-deaths # -recovered, but stopped reporting
+    recovered = deaths - deaths  # all zeros, since no longer reporting
+    return state, country, confirmed,recovered,deaths,current
+
+def getPlaceIndex(place,placeName):
+    ind = where(isin(place,placeName))[0]
+    return ind
+
+class Covid(object):
+    def __init__(self,placeName):
+        self._cache = [None, None] # cached results
+        self.placeName    = placeName
+        self.state, self.country, self.confirmed, self.recovered, self.deaths, self.current = getData(filePath)
+        if shape(where(isin(self.country,self.placeName)))[1] > 0:
+            self.ind = getPlaceIndex(self.country,self.placeName)
+            self.region = 'country'
+        else:
+            self.ind = getPlaceIndex(self.state,self.placeName)
+            self.region = 'state'
+
+    def getSeries(self,type,minCases=0):
+        if type=='confirmed':
+            localData = self.confirmed
+        if type=='deaths':
+            localData = self.deaths
+        if type=='recovered':
+            localData = self.recovered
+        if type=='current':
+            localData = self.current
+        part       = localData[self.ind,:] # keep localData associated with placeName
+        fullSeries = sum(part,axis=0)      # sum to get a single time series if localData is for a distributed country
+        keep = where(fullSeries>=minCases)
+        return fullSeries[keep]
+
+    def getLogD(self,type,minCases=0,frac=False):
+        series = self.getSeries(type,minCases)
+        Dseries = series[1:]-series[0:-1]
+        Aseries = (series[1:]+series[0:-1])/2
+        LD      = Dseries/Aseries
+        if frac:
+            LD = exp(LD)
+        return LD
+
+
+# smooth = np.convolve(Covid('Texas').getLogD('current',minCases=10,frac=True), np.ones((5,))/5, mode='valid')
